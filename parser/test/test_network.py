450a492cecd8736e274e6145e47a22d43edd41b0
Ethan Robert <ethan.rbrt90@gmail.com>
2025-10-24 01:15:23 +0200

    Splitted test_network into small functions to improve precision

--- parser/test/test_network.py
+++ parser/test/test_network.py
@@ -1,15 +1,34 @@
 from parser.network import validateIP
 
 
-def testAdress():
+def test_validate_ip_valid():
+    """Tests a valid CIDR IP address."""
+    assert validateIP("192.168.1.2/32") is True
 
-    assert validateIP("192.168.1.2/32") == True
-    assert validateIP("192.168.1.2/33") == False
-    assert validateIP("256.0.0.0/32") == False
-    assert validateIP("-1.0.0.0/32") == False
-    assert validateIP("0.0.0.0/-1") == False
-    assert validateIP("0.0.0.0") == False
-    assert validateIP("0.0.0/32") == False
-    assert validateIP("0.0.0.0.0/32") == False
+def test_validate_ip_invalid_mask_too_high():
+    """Tests a CIDR with a mask greater than 32."""
+    assert validateIP("192.168.1.2/33") is False
 
-    
+def test_validate_ip_invalid_mask_too_low():
+    """Tests a CIDR with a mask less than 0."""
+    assert validateIP("0.0.0.0/-1") is False
+
+def test_validate_ip_invalid_octet_too_high():
+    """Tests an IP with an octet greater than 255."""
+    assert validateIP("256.0.0.0/32") is False
+
+def test_validate_ip_invalid_octet_negative():
+    """Tests an IP with a negative octet."""
+    assert validateIP("-1.0.0.0/32") is False
+
+def test_validate_ip_missing_mask():
+    """Tests an IP address string without a CIDR mask."""
+    assert validateIP("0.0.0.0") is False
+
+def test_validate_ip_too_few_octets():
+    """Tests an IP with less than 4 octets."""
+    assert validateIP("0.0.0/32") is False
+
+def test_validate_ip_too_many_octets():
+    """Tests an IP with more than 4 octets."""
+    assert validateIP("0.0.0.0.0/32") is False
