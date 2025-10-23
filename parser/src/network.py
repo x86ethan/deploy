cd21f4d631bf06680938fe3c01943e2ee4f7803e
Ethan Robert <ethan.rbrt90@gmail.com>
2025-10-23 15:01:38 +0200

    Added a function to validate an IP adress

--- parser/src/network.py
+++ parser/src/network.py
@@ -3,7 +3,40 @@ from enum import Enum
 
 InterfaceType = Enum("InterfaceType", ["wireless", "wired"])
 
+def validateIP(ip: str):
 
+    # IP adresses are written under CIDR format 
+    try:
+        add = ip.split("/")[0].split(".")
+        mask = ip.split("/")[1]
+    except IndexError:
+        # The adress is not under CIDR format
+        return False
+
+    if len(add) != 4:
+        # This is not IPv4 format 
+        return False
+
+    for n in add:
+        try:
+            if int(n) > 255 or int(n) < 0:
+                # A litteral in the adress exceeds 255 or is below 0
+                return False
+        except ValueError:
+            # A litteral in the adress is not a number
+            return False
+        
+    try:
+        if int(mask) > 32 or int(mask) < 0:
+            # The mask exceeds 32 or is below 0
+            return False
+    except ValueError:
+        # The mask is not a number 
+        return False
+    
+    return True
+        
+    
         
     
 
